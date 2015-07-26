3c476f90ebfcb69fbfac22e41d15672842d760c4
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-07-27 00:22:50 +0300

    fix example

--- example.js
+++ example.js
@@ -3,24 +3,15 @@ var Blynk = require('./blynk');
 var blynk = new Blynk.Blynk('7736215262c242c1989a1e262fbbcb19');
 
 var v1 = new blynk.VirtualPin(1);
+var v9 = new blynk.VirtualPin(9);
 
-v1.on('write', function(param){
+v1.on('write', function(param) {
   console.log('V1:', param);
 });
 
-var v9 = new blynk.VirtualPin(9);
-v9.on('read', function(param){
+v9.on('read', function() {
   v9.write(new Date().getSeconds());
 });
 
-/*
-blynk.on('connected', function(){
-  setInterval(function() {
-    blynk.virtualWrite(9, ();
-  }, 1000);
-});
-*/
-
-blynk.on('disconnected', function(){
-  console.log("DISCONNECT");
-});
+blynk.on('connected', function() { console.log("Blynk ready."); });
+blynk.on('disconnected', function() { console.log("DISCONNECT"); });
