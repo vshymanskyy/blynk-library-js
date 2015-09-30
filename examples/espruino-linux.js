54baa237a6df58c9c7273f9f302b6e2550567949
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-09-30 16:08:38 +0300

    Add Espruino Linux example

---
+++ examples/espruino-linux.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env espruino
+var Blynk = require('blynk');
+
+var AUTH = 'YOUR_AUTH_TOKEN';
+
+var blynk = new Blynk.Blynk(AUTH, options = {
+  connector : new Blynk.EspruinoTCP()
+});
+
+var v1 = new blynk.VirtualPin(1);
+var v9 = new blynk.VirtualPin(9);
+
+v1.on('write', function(param) {
+  console.log('V1:', param);
+});
+
+v9.on('read', function() {
+  v9.write(new Date().getSeconds());
+});
+
+blynk.on('connect', function() { console.log("Blynk ready."); });
+blynk.on('disconnect', function() { console.log("DISCONNECT"); });
+
